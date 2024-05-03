a2374e1f302b6b93685a684fd8f1292d0279cc51
OciMM <bekasovmaks20@gmail.com>
2024-05-03 16:24:10 +0300

    login

--- storiesbro-frontend-main/src/components/UI/modals/EmailConfiramtionForm.jsx
+++ storiesbro-frontend-main/src/components/UI/modals/EmailConfiramtionForm.jsx
@@ -9,11 +9,23 @@ import GradientButton from "../buttons/GradientButton";
 import { API_URL } from "../../../constants/constatns";
 import axios from 'axios';
 
+import { useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { setTokken } from "../../../store/userReducer";
+
 // const CONFIRM_LINK = `${API_URL}activate/`;
 
 const EmailConfirmationForm = ({ isEmailConfirm, setIsEmailConfirm, userId }) => {
   const [error, setError] = useState(false);
   const [code, setCode] = useState("");
+  const [userSpecId, setUserId] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
 
   const handleConfirmForm = async () => {
     setIsEmailConfirm(false);
@@ -21,7 +33,40 @@ const EmailConfirmationForm = ({ isEmailConfirm, setIsEmailConfirm, userId }) =>
     try {
       console.log("Отправка запроса активации:", `${API_URL}activate/${userId}/${code}/`);
       const response = await axios.post(`${API_URL}activate/${userId}/${code}/`);
-      localStorage.setItem('statusActivate', true)
+      localStorage.setItem('statusActivate', true);
+      const email_lower = email.toLowerCase()
+      axios
+          .post(`${API_URL}login/`, {
+            email: email_lower,
+            password: password,
+          }, { withCredentials: true, headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"), }
+          })
+          .then(function (response) {
+            setUserId(response.data.id);
+            handleConfirmForm(response.data.id)
+              // setIsConfirmPageOpen(true);
+              // setIsLoginFormOpen(false);
+            axios.defaults.headers.common["Authorization"] =
+              "Bearer " + response.data["access"];
+
+            localStorage.setItem("token", response.data["access"]);
+            localStorage.setItem("refresh", response.data["refresh"])
+            localStorage.setItem("id", response.data["id"])
+            localStorage.setItem("count_of_visit", response.data["count_of_visit"] + 1)
+            dispatch(setTokken(response.data["access"]));
+              
+            localStorage.removeItem('statusActivate')
+            const checkStatus = localStorage.getItem("statusAccount");
+              if (checkStatus == "admin") {
+                navigate('/admin');
+              } if (checkStatus == "customer") {
+                navigate('/customer');
+              };
+            })
+            .catch(function (error) {
+              setError(true); // Устанавливаем флаг ошибки в true при ошибке запроса
+            });
       if (response.data.message) {
         console.log(response.data.message);
         // Возможно, вам нужно выполнить какие-то действия после успешной активации
