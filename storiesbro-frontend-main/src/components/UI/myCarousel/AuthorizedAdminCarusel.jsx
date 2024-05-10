fb1cd4172c31cb9dd769c9213d31e95b7fecc9bb
OciMM <bekasovmaks20@gmail.com>
2024-05-10 15:21:19 +0300

    fix width video

--- storiesbro-frontend-main/src/components/UI/myCarousel/AuthorizedAdminCarusel.jsx
+++ storiesbro-frontend-main/src/components/UI/myCarousel/AuthorizedAdminCarusel.jsx
@@ -4,21 +4,27 @@ import Carousel from "react-img-carousel";
 const AuthorizedAdminCarusel = ({ image }) => {
   require("react-img-carousel/lib/carousel.css");
 
+  const videoStyle = {
+    width: "258px",
+    height: "461px",
+    borderRadius: "10px", // Пример радиуса скругления
+  };
+
   return (
     <Carousel cellPadding={105}>
-      <video width="258px" height="461px" className="carouselItem" controls>
+      <video style={videoStyle} className="carouselItem" controls>
         <source src={image} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <video className="carouselItem" controls>
+      <video style={videoStyle} className="carouselItem" controls>
         <source src={image} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <video className="carouselItem" controls>
+      <video style={videoStyle} className="carouselItem" controls>
         <source src={image} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* Add more <video> elements for each video */}
+      {/* Добавьте больше элементов <video> для каждого видео */}
     </Carousel>
   );
 };
